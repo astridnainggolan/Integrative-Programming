529f5dffdd1974a43859a77a29452b26ae1e3b51
astridnainggolan <80021034+astridnainggolan@users.noreply.github.com>
2021-03-04 11:24:10 +0700

    Penjumlahan 2 variabel

---
+++ Tugas1.cpp
@@ -0,0 +1,15 @@
+include <iostream>
+using namespace std;
+
+int main()
+{
+int a = 10;
+int b = 2;
+
+cout<<"Nilai a = "<<a<<endl;
+cout<<"Nilai b = "<<b<<endl<<endl;
+
+cout<<"Hasil penjumlahan = "<<a+b<<endl;
+
+return 0;
+}
